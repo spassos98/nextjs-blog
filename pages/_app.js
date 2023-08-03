ebd70bb6154cbb640fdf69bf7a6cc7d3f820aae8
Santiago Passos <santiago.passos@tensoreye.de>
2023-08-03 14:26:23 -0500

    Revert "Update blog"
    This reverts commit a0091236740ba1d4786dd978d169b5a2c56ce60b.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
-import useOneSignal from '../lib/useOneSignal';
 import '../styles/global.css';
 
 export default function App({ Component, pageProps }) {
-    useOneSignal();
     return <Component {...pageProps} />;
 }
